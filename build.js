ea9a95c7d6435ccc2dbbdcff7da7f6cb4c350f8f
Adrien Ragot <aragot@play-sql.com>
2021-10-20 17:31:54 +0200

    Add 'npm install' as part of 'mvn install', and give tips if the html or js files are missing

--- build.js
+++ build.js
@@ -1,7 +1,14 @@
 const { build } = require("esbuild");
 const servor = require("servor");
 
+const watch = !!process.env.WATCH;
+
 const outDir = './target/classes/static/js';
+
+if (watch) {
+    console.log("\nKeep this program running while you work in your IDE (It rebuilds the JS files automatically)");
+    console.log("Press Ctrl+C to stop\n");
+}
 
 build({
     entryPoints: ['./src/main/resources/static/js/app.js'],
@@ -10,12 +17,11 @@ build({
     minify: true,
     bundle: true,
     sourcemap: true,
-    watch: true
+    watch: watch
 }).then(async () => {
-    await servor({
+    watch && await servor({
         browser: true,
         root: outDir,
         port: 8090,
     });
 });
-
--- src/main/java/hello/MvcConfig.java
+++ src/main/java/hello/MvcConfig.java
@@ -1,18 +1,31 @@
 package hello;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
+        checkIfFileExistsInClasspath("templates/home.html", "Is the folder src/main/resource considered a resource by your IDE?");
+        checkIfFileExistsInClasspath("static/js/app.js", "Did you run `npm run watch` in parallel?");
+
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+    }
+
+    private void checkIfFileExistsInClasspath(String expectedResource, String message) {
+        URL resource = this.getClass().getClassLoader().getResource(expectedResource);
+        if (resource == null) {
+            throw new RuntimeException(message + " The file target/classes/" + expectedResource + " is missing.");
+        }
     }
 
 }
